ba3acfe389a3ad2fe45199a218850367d3297ede
ArchanaVenugopal <31955897+ArchanaVenugopal@users.noreply.github.com>
2021-09-14 14:43:59 +0530

    Add files via upload

---
+++ +overload.py
@@ -0,0 +1,13 @@
+class complex:
+    def __init__(self):
+        self.n = int(input("Enter the real :"))
+        self.m= int(input("Enter the imag :"))
+        print("-----------------------------------------")
+        print()
+    def sum(self):
+
+        return self.n+self.m
+
+
+com1 = complex()
+com2 = complex()
